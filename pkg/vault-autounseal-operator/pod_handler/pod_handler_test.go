07959a96bac3fe30808f64167bfd32d62ae1e2fb
camaeel <12999736+camaeel@users.noreply.github.com>
2024-06-01 22:11:02 +0200

    another test

--- pkg/vault-autounseal-operator/pod_handler/pod_handler_test.go
+++ pkg/vault-autounseal-operator/pod_handler/pod_handler_test.go
@@ -66,7 +66,7 @@ func TestInitialize(t *testing.T) {
 	assert.Len(t, rootSecret.StringData, 1) //probably "feature" of fake client - check StringData instead of Data
 }
 
-func TestInitializeFailInitSecretExists(t *testing.T) {
+func TestInitializeFailOldInitSecretExists(t *testing.T) {
 	ctx := context.TODO()
 
 	fakeVault := vaultProvider.GetVault(t, false, 3)
@@ -104,6 +104,51 @@ func TestInitializeFailInitSecretExists(t *testing.T) {
 	err := initialize(slog.Default(), ctx, &cfg, fakeSecretLister, vaultNode)
 	assert.Error(t, err)
 	assert.EqualError(t, err, fmt.Sprintf("this pod isn't initialized yet, but initialization secret vault-autounseal-unlock-keys already exists and is older than %s - either this secret is old (from previous initialization) or initialization procedure failed", (3*cfg.InformerResync).String()))
+
+	sealed, initialized, err := vaultNode.GetSealStatus(ctx)
+
+	assert.NoError(t, err)
+	assert.False(t, initialized)
+	assert.True(t, sealed)
+}
+
+func TestInitializeFailRecentInitSecretExists(t *testing.T) {
+	ctx := context.TODO()
+
+	fakeVault := vaultProvider.GetVault(t, false, 3)
+	cfg := config.Config{
+		Namespace:              "vault",
+		HandlerTimeoutDuration: 30 * time.Second,
+		VaultTimeoutDuration:   10 * time.Second,
+		UnlockShares:           3,
+		UnlockThreshold:        2,
+		K8sClient:              fake.NewSimpleClientset(),
+		VaultUnlockKeysSecret:  "vault-autounseal-unlock-keys",
+		VaultRootTokenSecret:   "vault-autounseal-root-token",
+		InformerResync:         20 * time.Second,
+	}
+
+	fakeClient := fake.NewSimpleClientset(
+		&corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:              cfg.VaultUnlockKeysSecret,
+				Namespace:         cfg.Namespace,
+				CreationTimestamp: metav1.NewTime(time.Now().Add(-1 * time.Second)),
+			},
+			Data: map[string][]byte{},
+		},
+	)
+	fakeInformer := informers.NewSharedInformerFactoryWithOptions(fakeClient, 1, informers.WithNamespace(cfg.Namespace))
+	secretInformerFactory := fakeInformer.Core().V1().Secrets()
+	fakeSecretLister := secretInformerFactory.Lister()
+
+	fakeInformer.Start(ctx.Done())
+	fakeInformer.WaitForCacheSync(ctx.Done())
+
+	vaultNode := vaultProvider.Node{Client: fakeVault[0].Client}
+
+	err := initialize(slog.Default(), ctx, &cfg, fakeSecretLister, vaultNode)
+	assert.NoError(t, err)
 
 	sealed, initialized, err := vaultNode.GetSealStatus(ctx)
 
